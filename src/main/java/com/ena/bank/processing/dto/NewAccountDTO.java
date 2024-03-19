321a9303b3d67c27dadd215f58ec9ff5f882f93f
ena <erkembekov.nurzhan@mail.ru>
2024-03-19 10:03:08 +0500

    I have created account dto

---
+++ src/main/java/com/ena/bank/processing/dto/NewAccountDTO.java
@@ -0,0 +1,14 @@
+package com.ena.bank.processing.dto;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.Data;
+
+@Data
+public class NewAccountDTO {
+
+    @JsonAlias("currency")
+    private String currencyCode;
+
+    @JsonAlias("user")
+    private Long userId;
+}
