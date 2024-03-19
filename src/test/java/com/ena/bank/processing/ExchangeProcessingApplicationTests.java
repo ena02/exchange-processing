d89bae080fabe0da84d30aca3f3ac102631566fb
ena <erkembekov.nurzhan@mail.ru>
2024-03-19 06:42:22 +0500

    Initial commit

---
+++ src/test/java/com/ena/bank/processing/ExchangeProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ena.bank.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
