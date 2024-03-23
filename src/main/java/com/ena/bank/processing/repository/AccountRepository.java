0fed20608d01965144c63406dea9e024ba2db12e
ena <erkembekov.nurzhan@mail.ru>
2024-03-23 23:19:38 +0500

    add new method

--- src/main/java/com/ena/bank/processing/repository/AccountRepository.java
+++ src/main/java/com/ena/bank/processing/repository/AccountRepository.java
@@ -3,6 +3,8 @@ package com.ena.bank.processing.repository;
 import com.ena.bank.processing.model.AccountEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AccountRepository extends CrudRepository<AccountEntity, Long> {
+import java.util.List;
 
+public interface AccountRepository extends CrudRepository<AccountEntity, Long> {
+    List<AccountEntity> findByUserId(Long userId);
 }
