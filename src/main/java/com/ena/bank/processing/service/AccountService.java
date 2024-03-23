b91350a7f51dcdf43f305d29cb7d287529e455d4
ena <erkembekov.nurzhan@mail.ru>
2024-03-23 23:20:23 +0500

    add method getAccountById

--- src/main/java/com/ena/bank/processing/service/AccountService.java
+++ src/main/java/com/ena/bank/processing/service/AccountService.java
@@ -5,9 +5,12 @@ import com.ena.bank.processing.model.AccountEntity;
 import com.ena.bank.processing.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -27,6 +30,7 @@ public class AccountService {
         return created;
     }
 
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED)
     public AccountEntity addMoneyToAccount(String uid, Long accountId, BigDecimal money) {
         Optional<AccountEntity> account = accountRepository.findById(accountId);
         var result = account.map(acc -> {
@@ -35,5 +39,13 @@ public class AccountService {
             return accountRepository.save(acc);
         }).orElseThrow(() -> new IllegalArgumentException("Account with ID " + accountId + " not found"));
         return result;
+    }
+
+    public AccountEntity getAccountById(Long id) {
+        return accountRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Account with ID " + id + " not found"));
+    }
+
+    public List<AccountEntity> getAllAccount(Long userId) {
+        return accountRepository.findByUserId(userId);
     }
 }
